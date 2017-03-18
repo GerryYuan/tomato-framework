d4d92b0392de02f94365c3bfe4b3558457f4fca2
gerry <yuanguohua_7818@163.com>
2017-03-18 14:04:24 +0800

    添加plugin模块，以及demo添加dao依赖，注意，需要本地启动数据库服务

--- tomato-framework-demo/src/main/java/com/tomato/framework/demo/dao/DemoDAO.java
+++ tomato-framework-demo/src/main/java/com/tomato/framework/demo/dao/DemoDAO.java
@@ -1,23 +1,23 @@
-//package com.tomato.framework.demo.dao;
-//
-//import org.jfaster.mango.annotation.DB;
-//import org.jfaster.mango.annotation.SQL;
-//
-//import com.tomato.framework.demo.model.DemoMango;
-//
-//@DB(table = "mango")
-//public interface DemoDAO {
-//	String COLUMNS = "id, name";
-//
-//	@SQL("insert into #table(" + COLUMNS + ") values(:id, :name)")
-//	void addDemoMango(DemoMango demoMango);
-//
-//	@SQL("select " + COLUMNS + " from #table where id = :1")
-//	DemoMango getDemoMango(int id);
-//
-//	@SQL("update #table set name=:name where id = :id")
-//	boolean updateDemoMango(DemoMango demoMango);
-//
-//	@SQL("delete from #table where id = :1")
-//	boolean deleteDemoMango(int id);
-//}
+package com.tomato.framework.demo.dao;
+
+import org.jfaster.mango.annotation.DB;
+import org.jfaster.mango.annotation.SQL;
+
+import com.tomato.framework.demo.model.DemoMango;
+
+@DB(table = "mango")
+public interface DemoDAO {
+	String COLUMNS = "id, name";
+
+	@SQL("insert into #table(" + COLUMNS + ") values(:id, :name)")
+	void addDemoMango(DemoMango demoMango);
+
+	@SQL("select " + COLUMNS + " from #table where id = :1")
+	DemoMango getDemoMango(int id);
+
+	@SQL("update #table set name=:name where id = :id")
+	boolean updateDemoMango(DemoMango demoMango);
+
+	@SQL("delete from #table where id = :1")
+	boolean deleteDemoMango(int id);
+}
--- tomato-framework-demo/src/main/java/com/tomato/framework/demo/service/impl/DemoServiceImpl.java
+++ tomato-framework-demo/src/main/java/com/tomato/framework/demo/service/impl/DemoServiceImpl.java
@@ -1,17 +1,19 @@
 package com.tomato.framework.demo.service.impl;
 
+import com.tomato.framework.demo.dao.DemoDAO;
 import com.tomato.framework.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DemoServiceImpl implements DemoService {
 
-//	@Autowired
-//	private DemoDAO demoDAO;
+	@Autowired
+	private DemoDAO demoDAO;
 	
 	@Override
 	public String ping() {
-//		demoDAO.getDemoMango(1);
+		demoDAO.getDemoMango(1);
 		return "cs";
 	}
 
