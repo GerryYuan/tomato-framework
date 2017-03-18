996c3e18fd6bacb85943590242e6de160907641d
gerry <yuanguohua_7818@163.com>
2017-03-18 22:04:47 +0800

    get set lombok

--- tomato-framework-rest/src/main/java/com/tomato/framework/rest/result/page/PageResult.java
+++ tomato-framework-rest/src/main/java/com/tomato/framework/rest/result/page/PageResult.java
@@ -1,9 +1,11 @@
 package com.tomato.framework.rest.result.page;
 
+import com.tomato.framework.rest.result.ViewModelResult;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.List;
-
-import com.tomato.framework.rest.result.ViewModelResult;
 
 public class PageResult<T> implements Serializable {
 
@@ -13,57 +15,27 @@ public class PageResult<T> implements Serializable {
 	private static final long serialVersionUID = -6722719642632697152L;
 
 	// 根据页面显示的条数计算总页数
+	@Getter
+	@Setter
 	private int page;
 
+	@Getter
+	@Setter
 	private int pageSize;
 
 	// 根据传入的数据库查询数据库中的信息的条数
+	@Getter
+	@Setter
 	private int total;
 
 	// 返回结果集
+	@Getter
+	@Setter
 	private List<T> rows;
 
+	@Getter
+	@Setter
 	private boolean hasNext;
-
-	public boolean isHasNext() {
-		return hasNext;
-	}
-
-	public void setHasNext(boolean hasNext) {
-		this.hasNext = hasNext;
-	}
-
-	public int getPageSize() {
-		return pageSize;
-	}
-
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
-
-	public List<T> getRows() {
-		return rows;
-	}
-
-	public void setRows(List<T> rows) {
-		this.rows = rows;
-	}
-
-	public int getPage() {
-		return page;
-	}
-
-	public void setPage(int page) {
-		this.page = page;
-	}
-
-	public int getTotal() {
-		return total;
-	}
-
-	public void setTotal(int total) {
-		this.total = total;
-	}
 
 	public static <T, E> PageResult<T> buildPageResult(List<T> result, int rows, Pagination<E> pagination) {
 		PageResult<T> pageResult = new PageResult<T>();
