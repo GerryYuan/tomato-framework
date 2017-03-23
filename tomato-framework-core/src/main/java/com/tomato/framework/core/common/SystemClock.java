a58a481143e0b8cbaa57d4354f68f883cc740f45
gerry <yuanguohua_7818@163.com>
2017-03-24 00:19:24 +0800

    添加分布式高效有序id生产，每秒最多可生产418万个有序的ID，即QPS=400w/s，参考http://git.oschina.net/yu120/sequence

---
+++ tomato-framework-core/src/main/java/com/tomato/framework/core/common/SystemClock.java
@@ -0,0 +1,59 @@
+package com.tomato.framework.core.common;
+
+import java.sql.Timestamp;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Created by gerry
+ * @version 1.0, 2017-03-24-00:07
+ * @since com.hujiang 1.0.0
+ */
+public class SystemClock {
+    private final long period;
+    private final AtomicLong now;
+
+    private SystemClock(long period) {
+        this.period = period;
+        this.now = new AtomicLong(System.currentTimeMillis());
+        scheduleClockUpdating();
+    }
+
+    private static class InstanceHolder {
+        public static final SystemClock INSTANCE = new SystemClock(1);
+    }
+
+    private static SystemClock instance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private void scheduleClockUpdating() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
+            public Thread newThread(Runnable runnable) {
+                Thread thread = new Thread(runnable, "System Clock");
+                thread.setDaemon(true);
+                return thread;
+            }
+        });
+        scheduler.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                now.set(System.currentTimeMillis());
+            }
+        }, period, period, TimeUnit.MILLISECONDS);
+    }
+
+    private long currentTimeMillis() {
+        return now.get();
+    }
+
+    public static long now() {
+        return instance().currentTimeMillis();
+    }
+
+    public static String nowDate() {
+        return new Timestamp(instance().currentTimeMillis()).toString();
+    }
+}
--- tomato-framework-test/src/main/java/com/tomato/framework/test/IdUtilsTest.java
+++ tomato-framework-test/src/main/java/com/tomato/framework/test/IdUtilsTest.java
@@ -1,7 +1,9 @@
 package com.tomato.framework.test;
 
+import com.tomato.framework.core.util.IdUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,12 +13,13 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @since com.hujiang 1.0.0
  */
 @RunWith(SpringRunner.class)
-@PropertySource("")
-public class FrameworkTest {
+@ComponentScan(basePackages = {"com.tomato.framework"})
+@PropertySource("application-test.properties")
+public class IdUtilsTest {
 
     @Test
     public void test(){
-        System.out.printf("ddd");
+        System.out.print(new IdUtils(0,0).nextId());
     }
 
 }
