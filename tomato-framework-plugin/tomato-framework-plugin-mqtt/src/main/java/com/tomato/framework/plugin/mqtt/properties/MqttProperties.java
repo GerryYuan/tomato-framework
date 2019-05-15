a6916dad92ccfff819341429084001c939ae1bec
yuanguohua <yuanguohua@fin-shine.com>
2019-05-15 10:20:31 +0800

    Mqtt client不可重复，不然强制下线

--- tomato-framework-plugin/tomato-framework-plugin-mqtt/src/main/java/com/tomato/framework/plugin/mqtt/properties/MqttProperties.java
+++ tomato-framework-plugin/tomato-framework-plugin-mqtt/src/main/java/com/tomato/framework/plugin/mqtt/properties/MqttProperties.java
@@ -1,11 +1,16 @@
 package com.tomato.framework.plugin.mqtt.properties;
 
+import com.tomato.framework.plugin.mqtt.exception.MqttException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
 
 @Data
 @ConfigurationProperties(prefix = "com.mqtt")
 public class MqttProperties {
+    
     private String url;
     private String username;
     private String password;
@@ -17,16 +22,35 @@ public class MqttProperties {
     
     /**
      * #0代表“至多一次”，消息发布完全依赖底层 TCP/IP 网络。会发生消息丢失或重复。这一级别可用于如下情况，环境传感器数据，丢失一次读记录无所谓，因为不久后还会有第二次发送。
-     * #1代表“至少一次”，确保消息到达，但消息重复可能会发生。
-     * #2代表“只有一次”，确保消息到达一次。这一级别可用于如下情况，在计费系统中，消息重复或丢失会导致不正确的结果。
+     * #1代表“至少一次”，确保消息到达，但消息重复可能会发生。 #2代表“只有一次”，确保消息到达一次。这一级别可用于如下情况，在计费系统中，消息重复或丢失会导致不正确的结果。
      */
     private int qos = 1;
     /**
-     * #若心跳时间过短，在消费端繁忙时会无法完成服务端的心跳回复导致服务端认为消费端下线而无法完成订阅
-     * #可采用MQ接收端和消费端之间加上队列
+     * #若心跳时间过短，在消费端繁忙时会无法完成服务端的心跳回复导致服务端认为消费端下线而无法完成订阅 #可采用MQ接收端和消费端之间加上队列
      */
     private int keepAliveInterval = 60;
     
     private String defaultTopic;
     
+    public String getSubscribeId() {
+        if (StringUtils.isEmpty(subscribeId)) {
+            try {
+                return "sub@" + InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e) {
+                throw new MqttException(e);
+            }
+        }
+        return subscribeId;
+    }
+    
+    public String getPublisherId() {
+        if (StringUtils.isEmpty(publisherId)) {
+            try {
+                return "pub@" + InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e) {
+                throw new MqttException(e);
+            }
+        }
+        return publisherId;
+    }
 }
