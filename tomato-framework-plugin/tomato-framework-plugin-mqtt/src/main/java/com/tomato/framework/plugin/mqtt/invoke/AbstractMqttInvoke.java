4713181883a626c6c376bce272075cb59e0a907d
gerry <yuanguohua_7818@163.com>
2019-05-20 22:36:55 +0800

    优化mqtt监听者代码

--- tomato-framework-plugin/tomato-framework-plugin-mqtt/src/main/java/com/tomato/framework/plugin/mqtt/invoke/AbstractMqttInvoke.java
+++ tomato-framework-plugin/tomato-framework-plugin-mqtt/src/main/java/com/tomato/framework/plugin/mqtt/invoke/AbstractMqttInvoke.java
@@ -4,4 +4,10 @@ import org.springframework.beans.factory.InitializingBean;
 
 public abstract class AbstractMqttInvoke implements MqttInvoke, InitializingBean {
     
+    public abstract String[] listenTopics();
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        MqttInvokePlugin.getInstance().store(listenTopics(), this);
+    }
 }
