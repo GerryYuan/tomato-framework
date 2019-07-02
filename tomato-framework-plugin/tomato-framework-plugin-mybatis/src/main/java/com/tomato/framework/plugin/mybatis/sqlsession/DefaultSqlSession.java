5ea276b2d970ff3c56b0a75655ea969e8853a223
yuanguohua <yuanguohua@fin-shine.com>
2019-07-02 10:24:30 +0800

    手写mybatis-添加sql执行器

--- tomato-framework-plugin/tomato-framework-plugin-mybatis/src/main/java/com/tomato/framework/plugin/mybatis/sqlsession/DefaultSqlSession.java
+++ tomato-framework-plugin/tomato-framework-plugin-mybatis/src/main/java/com/tomato/framework/plugin/mybatis/sqlsession/DefaultSqlSession.java
@@ -1,11 +1,8 @@
 package com.tomato.framework.plugin.mybatis.sqlsession;
 
 import com.tomato.framework.plugin.mybatis.config.Configuration;
-import com.tomato.framework.plugin.mybatis.parse.BoundSql;
-import com.tomato.framework.plugin.mybatis.statement.MappedStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import com.tomato.framework.plugin.mybatis.excutor.SimpleSqlExcutor;
+import com.tomato.framework.plugin.mybatis.excutor.SqlExcutor;
 import java.util.List;
 
 /**
@@ -23,36 +20,16 @@ public class DefaultSqlSession implements SqlSession {
     
     @Override
     public <T> T selectOne(String statementId, Object param) {
-        MappedStatement mappedStatement = configuration.getMappedStatement(statementId);
-        //反射处理param，从ParamMapping中获取对应的变量名称
-        //1、如果是param类型是String类型或基本类型或包装类，则，直接赋值
-        //2、如果是一个Map类型，则直接从Map中查找对应的ParamMapping中的参数值
-        //3、如果是一个List类型，则直接迭代List.get(x)，进行赋值
-        //4、如果是一个Object类型，则需要反射，根据ParamMapping中的参数名，然后获取Object参数值
-        BoundSql boundSql = mappedStatement.getBoundSql();
-        try {
-            PreparedStatement preparedStatement = configuration.getDataSource().getConnection()
-                .prepareStatement(boundSql.getSql());
-            for (int i = 1; i <= boundSql.getParamMappings().size(); i++) {
-                if (param instanceof Integer) {
-                    preparedStatement.setInt(i, (Integer) param);
-                }
-            }
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1));
-                System.out.println(resultSet.getString(2));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        List<T> results = selectList(statementId, param);
+        if (results != null && results.size() == 1) {
+            return results.get(0);
         }
-        //TODO 通过执行器，执行sql
-        //TODO 处理结果集
         return null;
     }
     
     @Override
     public <T> List<T> selectList(String statementId, Object param) {
-        return null;
+        SqlExcutor sqlExcutor = new SimpleSqlExcutor(configuration, configuration.getMappedStatement(statementId));
+        return sqlExcutor.selectList(param);
     }
 }
